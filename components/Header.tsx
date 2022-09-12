dab58e16a52950661f5b3cc25194be98d7ea9ba7
Shimil Tharayil <shimiltharayil007@gmail.com>
2022-09-12 15:55:01 +0530

    Header complelted

---
+++ components/Header.tsx
@@ -0,0 +1,48 @@
+import  Image from "next/image";
+import { 
+    SearchIcon,
+    GlobeAltIcon,
+    MenuIcon,
+    UserIcon,
+    UserCircleIcon
+
+} from "@heroicons/react/solid"
+
+function Header() {
+  return (
+       <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+
+        {/* left div */}
+        <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        < Image
+        src="https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_B%C3%A9lo.svg"
+        layout="fill"
+        objectFit="contain"
+        objectPosition="left"
+
+         />
+        </div>
+
+        {/* Middle div */}
+
+        <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+              <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your Search" />
+              <SearchIcon  className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
+        </div>
+
+        {/* Right div */}
+        <div className="flex items-center space-x-4 justify-end text-gray-500">
+             <p>Become a host</p>
+             <GlobeAltIcon className="h-6 cursor-pointer" />
+             <div className="flex items-center">
+                <MenuIcon className="h-6 cursor-pointer"/>
+                <UserCircleIcon className="h-6 cursor-pointer"/>
+             </div>
+        </div>
+
+       </header>
+    
+  )
+}
+
+export default Header
